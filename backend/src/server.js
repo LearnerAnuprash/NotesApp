74e2e40b17740c06db588c9a8c10d4a7b3935067
LearnerAnuprash <info.anuprash@gmail.com>
2026-01-09 23:27:16 +0545

    Deploy: Production ready for platform 'render.com' using dynamic url and build commands

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import cors from "cors";
+import path from "path";
 
 // Configuring environment variables using dotenv
 import dotenv from "dotenv";
@@ -10,6 +11,7 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 //Middlewares
 // This must be used before actually calling the routes
@@ -17,11 +19,15 @@ const PORT = process.env.PORT || 5001;
 // appears
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  })
-);
+
+// If not in production and in local development
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
 
 app.use((req, _, next) => {
   // in this middleware, 'res' is never used , so replace with '_'
@@ -33,6 +39,15 @@ app.use(rateLimiter);
 
 //Use notesRoutes.js as notesRoutes
 app.use("/api/notes", notesRoutes);
+
+//Configuration for deployment
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.use((req, res) => {
+    res.status(404).json({ message: "Route not found" });
+  });
+}
 
 // This ensures that the database is connected before the server starts listening
 connectDB().then(() => {
